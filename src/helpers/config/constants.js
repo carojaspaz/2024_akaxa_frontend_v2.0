30240e2bb30af51ec9aeccd7560c9b286cb8e93d
Carlos Andrés Rojas Paz <carojaspaz@gmail.com>
2024-05-22 17:02:40 +0000

    Basic Skeleton

---
+++ src/helpers/config/constants.js
@@ -0,0 +1,12 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+const urlBase =  process.env.REACT_APP_API_URL || 'http://localhost:4000/api/v1/'
+const nodeEnv = process.env.NODE_ENV || 'development'
+export const Config = {
+    urlBase: urlBase,
+    nodeEnv: nodeEnv,
+    apiKey: 'AIzaSyCB8YbANdqp1484dMiVpKj_ofXLeXDHJ6w',
+    lat: 4,
+    lng: -72
+}
