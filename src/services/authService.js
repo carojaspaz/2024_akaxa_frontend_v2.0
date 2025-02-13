32c3d3cd591b06d66dd33b04136c6bd6659b9842
Carlos Andrés Rojas Paz <carojaspaz@gmail.com>
2025-02-13 01:55:24 +0000

    Fix Sesion

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,28 +1,38 @@
 /** @format */
-
-import axios from 'axios'
-
+import { BaseService } from './baseService'
 import { Config } from '../helpers/config/constants'
 
-export const loginService = async (email, password) => {
-  const startTime = Date.now()
-  try {
-    const payload = JSON.stringify({ email, password })
-    const response = await axios.post(`${Config.urlBase}/user/login`, payload, {
-      headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json',
-        'Accept-Language': 'es'
+const ErrorMessages = {
+  500: 'Internal Server Error',
+  404: 'Not Found',
+  400: 'Bad Request',
+}
+
+class LoginService extends BaseService {
+  constructor() {
+    super()
+  }
+
+  /**
+   * Método para iniciar sesion
+   * @param {string} body - Los datos del cliente en formato JSON
+   * @returns {Promise<Object>} - Promesa que resuelve con la respuesta del servidor
+   */
+  async login(body) {
+    try {
+      const response = await fetch(Config.urlBase + '/user/login', this.optionsSimplePost(JSON.stringify(body)))
+
+      if (!response.ok) {
+        throw new Error(`HTTP Error status: ${response.status}`)
       }
-    })
-    return response.data
-  } catch (error) {
-    if (error.response) {
-      const { status } = error.response
-      if (status === 403 || status === 404) {
-        throw new Error(error.response.data.message)
-      }
+
+      const data = await response.json()
+      return data
+    } catch (error) {
+      console.error('Error al crear el cliente:', error)
+      throw error
     }
-    throw new Error('An error occurred')
   }
 }
+
+export const loginService = new LoginService()
